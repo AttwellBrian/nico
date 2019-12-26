8d850d544585e23be7a1169b6e013413440f4081
Johnson Fung <me@johnsonfung.com>
2019-12-26 23:25:05 +0800

    Added side modules and fake data
    Added topbar, user, competition, resources, and power plants sidebars. Added all powerplants in fake data and fully modeled fake API schema. Added bootstrap and reactstrap includes for easy row/column/modals/tabs.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,81 +1,181 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 import mainMapImage from "./assets/images/usmap-background.jpg";
-import { map } from "./fakeData";
+import { map, userProfile, gameState, powerPlants } from "./fakeData";
 import City from "./components/City";
 import ConnectionPipe from "./components/ConnectionPipe";
 import * as tools from "./functions";
 
-function App() {
-  // Prep map data
-  var citiesArray = [];
-  var connectionsArray = [];
-
-  for (var key in map.cities) {
-    // add all cities to an array
-    citiesArray.push(map.cities[key]);
-
-    // add all connections to an array
-    for (var connection in map.cities[key].connections) {
-      connectionsArray.push({
-        startCity: key,
-        endCity: map.cities[key].connections[connection].id,
-        cost: map.cities[key].connections[connection].cost
-      });
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userProfile: {
+        name: "",
+        color: "",
+        uuid: "" // uuidABC
+      },
+      gameState: {
+        players: {
+          uuidABC: {
+            name: "",
+            color: ""
+          }
+        },
+        playerOrder: [
+          /*
+          "uuidABC",
+          "uuidABC2"
+          */
+        ],
+        currentPlayer: "", // uuid
+        ownedPowerPlants: {
+          /* 
+          uuidABC: [
+            "plantUUID",
+            "plantUUID",
+            "plantUUID",
+            "plantUUID"
+          ]
+          */
+        },
+        ownedResources: {
+          /*
+          uuidABC: {
+            money: 95,
+            coal: 25,
+            garbage: 15,
+            uranium: 12,
+            oil: 22
+          }
+          */
+        },
+        ownedCities: {
+          /*
+          uuidABC: [
+            "cityID",
+            "cityID",
+            "cityID",
+            "cityID"
+          ]
+          */
+        },
+        actualMarket: [
+          /*
+          "plantUUID",
+          "plantUUID",
+          "plantUUID",
+          "plantUUID",
+          */
+        ],
+        futureMarket: [
+          /*
+          "plantUUID",
+          "plantUUID",
+          "plantUUID",
+          "plantUUID",
+          */
+        ]
+      },
+      powerPlants: {}
+    };
   }
 
-  var cities = citiesArray.map((city, index) => {
-    return (
-      <City
-        key={index}
-        name={city.name}
-        id={city.id}
-        top={city.top}
-        left={city.left}
-      />
-    );
-  });
+  componentDidUpdate() {}
 
-  // Pipe Connections
-  // remove repeats
-  for (var i in connectionsArray) {
-    for (var t in connectionsArray) {
-      if (
-        connectionsArray[i].startCity === connectionsArray[t].endCity &&
-        connectionsArray[i].endCity === connectionsArray[t].startCity
-      ) {
-        connectionsArray.splice(t, 1);
+  componentDidMount() {
+    this.setState({
+      userProfile: userProfile,
+      gameState: gameState,
+      powerPlants: powerPlants
+    });
+  }
+
+  render() {
+    // Prep map data
+    var citiesArray = [];
+    var connectionsArray = [];
+
+    for (var key in map.cities) {
+      // add all cities to an array
+      citiesArray.push(map.cities[key]);
+
+      // add all connections to an array
+      for (var connection in map.cities[key].connections) {
+        connectionsArray.push({
+          startCity: key,
+          endCity: map.cities[key].connections[connection].id,
+          cost: map.cities[key].connections[connection].cost
+        });
       }
     }
-  }
-  // render connections
-  var allConnections = connectionsArray.map((connection, index) => {
-    return (
-      <ConnectionPipe
-        key={index}
-        startCity={connection.startCity}
-        endCity={connection.endCity}
-        cost={connection.cost}
-      />
-    );
-  });
 
-  return (
-    <div className="App">
-      <div className="connections">
-        <div className="connectionPipesContainer">{allConnections} </div>
-      </div>
-      <div className="mainMap">
-        {cities}
-        <img
-          className="mainMapImage"
-          alt="Map of US Background"
-          src={mainMapImage}
+    var cities = citiesArray.map((city, index) => {
+      return (
+        <City
+          key={index}
+          name={city.name}
+          id={city.id}
+          top={city.top}
+          left={city.left}
         />
+      );
+    });
+
+    // Pipe Connections
+    // remove repeats
+    for (var i in connectionsArray) {
+      for (var t in connectionsArray) {
+        if (
+          connectionsArray[i].startCity === connectionsArray[t].endCity &&
+          connectionsArray[i].endCity === connectionsArray[t].startCity
+        ) {
+          connectionsArray.splice(t, 1);
+        }
+      }
+    }
+    // render connections
+    var allConnections = connectionsArray.map((connection, index) => {
+      return (
+        <ConnectionPipe
+          key={index}
+          startCity={connection.startCity}
+          endCity={connection.endCity}
+          cost={connection.cost}
+        />
+      );
+    });
+
+    return (
+      <div className="App">
+        <div className="topBar"></div>
+        <div className="resourcesContainer">
+          <div className="containerTitle">Resources Market</div>
+        </div>
+        <div className="userContainer">
+          <div className="containerTitle">{userProfile.name}</div>
+        </div>
+
+        <div className="marketContainer">
+          <div className="containerTitle">Powerplant Market</div>
+        </div>
+        <div className="playersContainer">
+          <div className="containerTitle">The Competition</div>
+        </div>
+        <div className="connections">
+          <div className="connectionPipesContainer">{allConnections} </div>
+        </div>
+        <div className="mainMap">
+          {cities}
+          <img
+            className="mainMapImage"
+            alt="Map of US Background"
+            src={mainMapImage}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- frontend/src/fakeData.js
+++ frontend/src/fakeData.js
@@ -93,4 +93,320 @@ var map = {
   }
 };
 
-export { map };
+var powerPlants = {
+  1: {
+    type: "oil",
+    cost: 3,
+    resourcesNeeded: 2,
+    citiesPowered: 1
+  },
+  2: {
+    type: "coal",
+    cost: 4,
+    resourcesNeeded: 2,
+    citiesPowered: 1
+  },
+  3: {
+    type: "hybrid",
+    cost: 5,
+    resourcesNeeded: 2,
+    citiesPowered: 1
+  },
+  4: {
+    type: "garbage",
+    cost: 6,
+    resourcesNeeded: 1,
+    citiesPowered: 1
+  },
+  5: {
+    type: "oil",
+    cost: 7,
+    resourcesNeeded: 3,
+    citiesPowered: 2
+  },
+  6: {
+    type: "coal",
+    cost: 8,
+    resourcesNeeded: 3,
+    citiesPowered: 2
+  },
+  7: {
+    type: "oil",
+    cost: 9,
+    resourcesNeeded: 1,
+    citiesPowered: 1
+  },
+  8: {
+    type: "coal",
+    cost: 10,
+    resourcesNeeded: 2,
+    citiesPowered: 2
+  },
+  9: {
+    type: "uranium",
+    cost: 11,
+    resourcesNeeded: 1,
+    citiesPowered: 2
+  },
+  10: {
+    type: "hybrid",
+    cost: 12,
+    resourcesNeeded: 2,
+    citiesPowered: 2
+  },
+  11: {
+    type: "ecological",
+    cost: 13,
+    resourcesNeeded: 0,
+    citiesPowered: 1
+  },
+  12: {
+    type: "garbage",
+    cost: 14,
+    resourcesNeeded: 2,
+    citiesPowered: 2
+  },
+  13: {
+    type: "coal",
+    cost: 15,
+    resourcesNeeded: 2,
+    citiesPowered: 3
+  },
+  14: {
+    type: "oil",
+    cost: 16,
+    resourcesNeeded: 2,
+    citiesPowered: 3
+  },
+  15: {
+    type: "uranium",
+    cost: 17,
+    resourcesNeeded: 1,
+    citiesPowered: 2
+  },
+  16: {
+    type: "ecological",
+    cost: 18,
+    resourcesNeeded: 0,
+    citiesPowered: 2
+  },
+  17: {
+    type: "garbage",
+    cost: 19,
+    resourcesNeeded: 2,
+    citiesPowered: 3
+  },
+  18: {
+    type: "coal",
+    cost: 20,
+    resourcesNeeded: 3,
+    citiesPowered: 5
+  },
+  19: {
+    type: "hybrid",
+    cost: 21,
+    resourcesNeeded: 2,
+    citiesPowered: 4
+  },
+  20: {
+    type: "ecological",
+    cost: 22,
+    resourcesNeeded: 0,
+    citiesPowered: 2
+  },
+  21: {
+    type: "uranium",
+    cost: 23,
+    resourcesNeeded: 1,
+    citiesPowered: 3
+  },
+  22: {
+    type: "garbage",
+    cost: 24,
+    resourcesNeeded: 2,
+    citiesPowered: 4
+  },
+  23: {
+    type: "coal",
+    cost: 25,
+    resourcesNeeded: 2,
+    citiesPowered: 5
+  },
+  24: {
+    type: "oil",
+    cost: 26,
+    resourcesNeeded: 2,
+    citiesPowered: 5
+  }
+  25: {
+    type: "ecological",
+    cost: 27,
+    resourcesNeeded: 3,
+    citiesPowered: 1
+  },
+  26: {
+    type: "uranium",
+    cost: 28,
+    resourcesNeeded: 1,
+    citiesPowered: 4
+  },
+  27: {
+    type: "hybrid",
+    cost: 29,
+    resourcesNeeded: 1,
+    citiesPowered: 4
+  },
+  28: {
+    type: "garbage",
+    cost: 30,
+    resourcesNeeded: 3,
+    citiesPowered: 6
+  },
+  29: {
+    type: "coal",
+    cost: 31,
+    resourcesNeeded: 3,
+    citiesPowered: 6
+  },
+  30: {
+    type: "oil",
+    cost: 32,
+    resourcesNeeded: 3,
+    citiesPowered: 6
+  },
+  31: {
+    type: "ecological",
+    cost: 33,
+    resourcesNeeded: 0,
+    citiesPowered: 4
+  },
+  32: {
+    type: "uranium",
+    cost: 34,
+    resourcesNeeded: 1,
+    citiesPowered: 5
+  },
+  33: {
+    type: "oil",
+    cost: 35,
+    resourcesNeeded: 1,
+    citiesPowered: 5
+  },
+  34: {
+    type: "coal",
+    cost: 36,
+    resourcesNeeded: 3,
+    citiesPowered: 7
+  },
+  35: {
+    type: "ecological",
+    cost: 37,
+    resourcesNeeded: 0,
+    citiesPowered: 4
+  },
+  36: {
+    type: "garbage",
+    cost: 38,
+    resourcesNeeded: 3,
+    citiesPowered: 7
+  },
+  37: {
+    type: "uranium",
+    cost: 39,
+    resourcesNeeded: 1,
+    citiesPowered: 6
+  },
+  38: {
+    type: "oil",
+    cost: 40,
+    resourcesNeeded: 2,
+    citiesPowered: 6
+  },
+  39: {
+    type: "coal",
+    cost: 42,
+    resourcesNeeded: 2,
+    citiesPowered: 6
+  },
+  40: {
+    type: "ecological",
+    cost: 44,
+    resourcesNeeded: 0,
+    citiesPowered: 5
+  },
+  41: {
+    type: "hybrid",
+    cost: 46,
+    resourcesNeeded: 3,
+    citiesPowered: 7
+  },
+  42: {
+    type: "ecological",
+    cost: 50,
+    resourcesNeeded: 0,
+    citiesPowered: 6
+  }
+
+};
+
+var userProfile = {
+  name: "Lightning Bug, LLC.",
+  color: "blue",
+  uuid: "PLYR1"
+};
+
+var gameState = {
+  players: {
+    PLYR1: {
+      name: "Lightning Bug, LLC.",
+      color: "blue"
+    },
+    PLYR2: {
+      name: "Oil Barron, Inc.",
+      color: "green"
+    },
+    PLYR3: {
+      name: "Armco, Co.",
+      color: "red"
+    }
+  },
+  playerOrder: ["PLYR2", "PLYR3", "PLYR1"],
+  currentPlayer: "PLYR1",
+  ownedPowerPlants: {
+    PLYR1: ["1", "2", "3"],
+    PLYR2: ["4", "5", "6"],
+    PLYR3: ["7", "8", "9"]
+  },
+  ownedResources: {
+    PLYR1: {
+      money: 40,
+      coal: 5,
+      garbage: 2,
+      uranium: 1,
+      oil: 3
+    },
+    PLYR2: {
+      money: 20,
+      coal: 1,
+      garbage: 0,
+      uranium: 3,
+      oil: 2
+    },
+    PLYR3: {
+      money: 95,
+      coal: 5,
+      garbage: 1,
+      uranium: 5,
+      oil: 10
+    }
+  },
+  ownedCities: {
+    PLYR1: ["seattle"],
+    PLYR2: ["boise"],
+    PLYR3: ["billings"]
+  },
+  actualMarket: ["10", "11", "12", "13"],
+  futureMarket: ["14", "15", "16", "17"]
+};
+
+export { map, userProfile, gameState, powerPlants };
