aa38668160724553868138863c99c76894c7f453
Johnson Fung <me@johnsonfung.com>
2020-01-06 15:33:33 +0700

    Began connecting bid buttons to server

--- frontend/src/functions.js
+++ frontend/src/functions.js
@@ -277,13 +277,18 @@ function apiStartGame(loginScreen, parent) {
     });
 }
 
-function apiAuctionAction() {
-  // note for format later: {"userId":"d108e378-ee12-441a-bc3a-524db44a4529","actionType":"CHOOSE_PLANT", "choosePlantId":"b182b03f-b23c-463e-8f6c-0abe063c1aa2", "bid": 1}
-}
-
 // API CALLS
-function bidOnPowerPlant(userId, plantId) {
-  alert(userId + " bid on power plant " + plantId);
+function apiAuctionAction(actionType, userId, plantId, bid) {
+  const auctionData = {
+    actionType: actionType,
+    userId: userId,
+    choosePlantId: plantId,
+    bid: bid
+  };
+  axios
+    // get data from server
+    .post(constants.serverURL + "/auction/act", auctionData)
+    .then(data => {});
 }
 
 function resetPlayers(that) {
@@ -311,7 +316,6 @@ export {
   generateMap,
   apiUpdateState,
   apiUpdateStateInterval,
-  bidOnPowerPlant,
   resetPlayers,
   newPlayer,
   apiCreateUser,
